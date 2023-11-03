e6921ebcffc2979f8259f7f2b62e303f18b2f93e
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-11-03 12:52:52 +0200

    Update subspace2.sh

--- subspace2.sh
+++ subspace2.sh
@@ -31,9 +31,10 @@ echo -e '                                       |___/ '
 echo -e '\e[0m'
 sleep 1 
 
-sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 jq -y
 cd $HOME
-wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.8-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.6-alpha 
+PULSAR_LATEST_TAG=$(curl -s https://api.github.com/repos/subspace/pulsar/tags | jq -r '.[0].name')
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/$PULSAR_LATEST_TAG/pulsar-ubuntu-x86_64-skylake-$PULSAR_LATEST_TAG
 sudo chmod +x pulsar
 sudo mv pulsar /usr/local/bin/
 sudo rm -rf $HOME/.config/pulsar
